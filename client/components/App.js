9f15022ae7024c3fa3fcb46c3e2800b2a1248037
Ellie K <61605543+king-ellie@users.noreply.github.com>
2021-09-05 12:51:17 -0400

    added event list

--- client/components/App.js
+++ client/components/App.js
@@ -1,17 +1,51 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 
-function HooksComponent() {
-  const [state, setState] = useState("starting state");
+function App() {
+  const [events, setEvents] = useState(null);
 
   useEffect(() => {
-    // function that executes upon component mounting
-  });
+    const fetchData = async() => {
+      const currTimestamp = Math.floor((Date.now()) / 1000)
+      const response = (await axios.get(`https://api.mobilize.us/v1/events?timeslot_start=gte_${currTimestamp}&per_page=15`)).data
+      const { data } = response
+      
+      setEvents(data)
+    }
+    fetchData()
+
+  }, []);
+
 
   return (
-    <h2>This is your hooks component!</h2>
+    <div>
+      <h1>Take Action!</h1>
+      <h3>Upcoming Events</h3>
+
+      <ul>
+        { events ? 
+          <ul> {events.map((event, idx) => {
+            const { title, sponsor, location, timeslots, browser_url } = event
+
+            let firstTimeSlot = (new Date(timeslots[0].start_date * 1000)).toString()
+
+            return (
+              <li key={idx} className="event-listing">
+                <h3>{title}</h3>
+                <h4>{sponsor.name} | {event.is_virtual ? "Virtual" : location.region}</h4>
+                <p>{firstTimeSlot} {timeslots.length > 1 ? `and ${timeslots.length} more dates` : null} </p>
+                <a href={`${browser_url}`}>Go to Event</a>
+              </li>
+            )})}
+          </ul>
+
+        : <h1>Loading</h1>
+      }
+      </ul>
+    </div>
   )
 }
 
-export default HooksComponent
+export default App
